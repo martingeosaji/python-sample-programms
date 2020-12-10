9182db30c1b245ede63c1f7a6547a0392a53f54b
martingeosaji <martingeo97@gmail.com>
2020-12-10 10:48:54 +0530

    updated

--- small.py
+++ small.py
@@ -1,6 +1,6 @@
-num1=7
-num2=4
-num3=5
+num1=int(input("enter the first number"))
+num2=int(input("enter the second number"))
+num3=int(input("enter the third number"))
 if(num1<num2):
     if(num1<num3):
         print(num1)
