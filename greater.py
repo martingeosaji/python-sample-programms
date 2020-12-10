2860f57a54e7016e98859bb2790188a4a7f73fa7
martingeosaji <martingeo97@gmail.com>
2020-12-10 10:47:25 +0530

    updated

--- greater.py
+++ greater.py
@@ -1,6 +1,6 @@
-num1=4
-num2=1
-num3=7
+num1=int(input("enter the first number"))
+num2=int(input("enter the second number"))
+num3=int(input("enter the third number"))
 if(num1>num2):
  if(num1>num3):
     print(num1)
